c531aa650c91e860e90fa8497db1f56f835e93cc
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-14 19:40:16 +0100

    removed default database values

--- Database/todolist_schema.sql
+++ Database/todolist_schema.sql
@@ -1,9 +1,4 @@
 CREATE TABLE todos (
-    id int PRIMARY KEY,
+    id uuid PRIMARY KEY,
     text varchar(2000) NOT NULL
 );
-
-INSERT INTO todos VALUES (1, 'Quarkus Backend');
-INSERT INTO todos VALUES (2, 'MySQL Database');
-INSERT INTO todos VALUES (3, 'Essen');
-INSERT INTO todos VALUES (4, 'Frontend');
